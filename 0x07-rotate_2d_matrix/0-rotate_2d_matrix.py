e806b0cc77b1ed442a8169ea7b94b3b6b8579a9e
Michael Wekesa <wekesa360@outlook.com>
2022-05-28 21:20:01 +0300

    rotate 2d matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,17 @@
+"""
+Given an n x n 2D matrix, rotate it 90 degrees clockwise.
+"""
+
+def rotate_2d_matrix(matrix):
+    rotated_matrix = []
+    m_len = len(matrix)
+    for i in range(m_len):
+        for m in matrix:
+            rotated_matrix.append(m[i])
+        n = rotated_matrix[::-1]
+    n_len = len(n)
+    chunk_size = len(matrix[0])
+    rotated_matrix = list()
+    for i in range(0, n_len, chunk_size):
+        rotated_matrix.append(n[i:i+chunk_size])
+    return rotated_matrix[::-1]
