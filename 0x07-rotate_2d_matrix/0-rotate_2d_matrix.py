ad34f4a3f4a5868a454f3571e553f23998fb855a
Michael Wekesa <wekesa360@outlook.com>
2022-05-28 21:49:43 +0300

    change logic

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,16 +3,8 @@ Given an n x n 2D matrix, rotate it 90 degrees clockwise.
 """
 
 def rotate_2d_matrix(matrix):
-    rotated_matrix = []
-    m_len = len(matrix)
-    for i in range(m_len):
-        for m in matrix:
-            rotated_matrix.append(m[i])
-        n = rotated_matrix[::-1]
-    n_len = len(n)
-    chunk_size = len(matrix[0])
-    rotated_matrix = list()
-    for i in range(0, n_len, chunk_size):
-        rotated_matrix.append(n[i:i+chunk_size])
+    rotate_matrix = []
+    for i in range(0, len(matrix)):
+        rotate_matrix.append([j[i] for j in reversed(matrix)])
     matrix.clear()
-    matrix += rotated_matrix[::-1]
+    matrix += rotate_matrix
